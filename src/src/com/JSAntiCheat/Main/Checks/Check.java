a6d62d7989c2f2db3474054e5364da8ef63b3eee
semdejong <115355@isendoorn.nl>
2020-05-01 14:57:36 +0100

    Added packages and made start to setting the project up!

--- src/src/com/JSAntiCheat/Main/Checks/Check.java
+++ src/src/com/JSAntiCheat/Main/Checks/Check.java
@@ -1,4 +1,13 @@
 package com.JSAntiCheat.Main.Checks;
 
+import com.JSAntiCheat.Main.JSAntiCheatMain;
+import org.bukkit.craftbukkit.Main;
+
 public class Check {
+
+    JSAntiCheatMain plugin = JSAntiCheatMain.getPlugin(JSAntiCheatMain.class);
+
+
+
+
 }
--- src/src/com/JSAntiCheat/Main/JSAntiCheatMain.java
+++ src/src/com/JSAntiCheat/Main/JSAntiCheatMain.java
@@ -3,11 +3,15 @@ package com.JSAntiCheat.Main;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+
 public class JSAntiCheatMain extends JavaPlugin  implements Listener {
+
+    public HashMap<String, String> test;
 
     @Override
     public void onEnable() {
-
+        test = new HashMap<>();
     }
 
     public void onDisable() {
